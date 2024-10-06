e698a75e378a86373a6cbc9c7826d1e2018bc0bd
hangang1 <152799387+hangang1@users.noreply.github.com>
2024-10-06 23:39:25 +0900

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+  apiKey: "AIzaSyAoHvXROvZkPvkSwBBKjUbBoZhoDdDyLFE",
+  authDomain: "vanila-pre.firebaseapp.com",
+  databaseURL: "https://vanila-pre-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "vanila-pre",
+  storageBucket: "vanila-pre.appspot.com",
+  messagingSenderId: "124683276156",
+  appId: "1:124683276156:web:0786b1d84890d60b041ef9",
+  measurementId: "G-Q8TJ5L2LFN"
+};
